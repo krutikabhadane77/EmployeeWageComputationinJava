85479b3a101e11125945b740ee26efa259b941cf
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-19 22:32:19 +0530

    Added: UC-2 Calculate Daily Employee Wage

---
+++ CalculateDailywage.java
@@ -0,0 +1,22 @@
+package EmployeeWageComputation;
+
+public class CalculateDailywage {
+    public static void main(String args[])
+    {
+        final int FULL_TIME = 1;
+        int WAGE_PER_HR = 20;
+        int workingHours = 0;
+        double empType = Math.floor(Math.random() * 10) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+            workingHours = 8;
+        } else
+        {
+            System.out.println("Employee is Absent");
+        }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
+    }
+
+}
